e218d8e771f5579a5d0386d5ada078b51b89db5c
hasanelhaj <sehasanelhaj@gmail.com>
2024-04-08 14:37:54 -0700

    Yes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THIS IS THE PRACTICE PROJECT <code></code> _______
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a sample Project for my Portfolio
         </a>
       </header>
     </div>
